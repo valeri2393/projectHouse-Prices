ab32db8705429c9d7b9197c2f3d4e60e80f6b0f2
Valeriya Aleshnikova <160396539+valeri2393@users.noreply.github.com>
2024-06-07 16:15:58 +0300

    Add files via upload

---
+++ app_1.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+import joblib
+
+# Загрузка модели
+model = LinearRegression()
+# model.fit(X_train, y_train)
+# joblib.dump(model, 'your_model.pkl')
+loaded_model = joblib.load('your_model.pkl')
+
+# Интерфейс для загрузки файла
+st.title("Оценка стоимости недвижимости. Модель - LinearRegression")
+st.write("Загрузите файл с фичами для получения оценки стоимости")
+
+uploaded_file = st.file_uploader("Выберите файл", type=["csv"])
+
+if uploaded_file is not None:
+    # Чтение загруженного файла
+    input_data = pd.read_csv(uploaded_file)
+    
+    # Отображение загруженных данных
+    st.write("Загруженные данные:")
+    st.write(input_data)
+    
+     # Предсказание
+    predictions = loaded_model.predict(input_data)
+    
+    # Отображение предсказаний
+    st.write("Предсказания стоимости:")
+    st.write(predictions)
+
